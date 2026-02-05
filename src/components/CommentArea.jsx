d9ee5bc3a305c6015840d8c5794accfc1ce3e558
Dedes <federico.lepore73@gmail.com>
2026-02-05 19:41:06 +0100

    CommentAre

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,8 +1,47 @@
+import { Component } from "react";
 
+// variabili componente
+const fetchURL = `https://striveschool-api.herokuapp.com/api/comments/`;
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTczM2Q0Njg1ZTNiMTAwMTViNWVkOTAiLCJpYXQiOjE3NzAzMDMyOTYsImV4cCI6MTc3MTUxMjg5Nn0.J_uMfQej1JyBhgkd_AVpMD9K7vyOqzIbX-ddI3JhOLc"
 
 class CommentArea extends Component {
+
+    state = {
+        comments: []
+    }
+
+    getComments() {
+
+        fetch(fetchURL + this.props.asin, {
+            headers: {
+                Authorization: "Bearer " + key
+            }
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json()
+                } else {
+                    throw new Error("Errore nel recupero dei commenti")
+                }
+            })
+            .then((data) => {
+                this.setState({ comments: data })
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    };
+
+    componentDidMount() {
+        this.getComments()
+    }
+
     render() {
         return (
+            <>
+                <CommentsList list={this.state.comments} />
+                <AddComment asin={this.props.asin} refreshComments={this.getComments} />
+            </>
 
         )
     }
