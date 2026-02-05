d74e3517e55e177a721c390f240fd753ad611246
Dedes <federico.lepore73@gmail.com>
2026-02-05 19:06:56 +0100

    SingleBook

---
+++ src/components/SingleBook.jsx
@@ -0,0 +1,34 @@
+import { Component } from "react";
+import { Card } from "react-bootstrap";
+import Col from "react-bootstrap";
+import CommentArea from "./CommentArea.jsx";
+
+class SingleBook extends Component {
+    render() {
+        return (
+            <>
+                <Col xs={12} md={6} lg={4} xl={3} key={this.props.asin}>
+                    <Card className="mb-3"
+                        style={this.state.selected ? { boxShadow: "0 0 10px blue" } : {}}
+                        onClick={() => this.setState({ selected: !this.state.selected })}
+                    >
+                        <Card.Img variant="top" src={this.props.img} className="object-fit-cover" />
+                        <Card.Body>
+                            <Card.Title>{this.props.title}</Card.Title>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <Card.Text className="m-0">{this.props.category}</Card.Text>
+                                <Card.Text>€{this.props.price}</Card.Text>
+                            </div>
+                        </Card.Body>
+                    </Card>
+
+                    {this.state.selected && <CommentArea asin={this.props.asin} />}
+                </Col>
+
+
+            </>
+        )
+    }
+}
+
+export default SingleBook;
